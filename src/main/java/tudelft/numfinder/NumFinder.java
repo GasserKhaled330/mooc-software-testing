e93ae6b299a9e7409ad1212f1cfc23d9774e733b
gasser <gasserkhaled23@gmail.com>
2023-10-15 21:50:56 +0300

    fix: fixing a bug in NumFinder class

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -1,25 +1,27 @@
 package tudelft.numfinder;
 
+import org.jetbrains.annotations.NotNull;
+
 public class NumFinder {
     private int smallest = Integer.MAX_VALUE;
     private int largest = Integer.MIN_VALUE;
 
-    public void find(int[] nums) {
-        for(int n : nums) {
+    public void find(@NotNull int[] nums) {
+        for (int n : nums) {
 
-            if(n < smallest)
+            if (n < smallest)
                 smallest = n;
-            else if (n > largest)
+            if (n > largest)
                 largest = n;
 
         }
     }
 
-    public int getSmallest () {
+    public int getSmallest() {
         return smallest;
     }
 
-    public int getLargest () {
+    public int getLargest() {
         return largest;
     }
 }
